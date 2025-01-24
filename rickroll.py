048cdce5fdc71b05f38070e54c915e3b4f86d86a
izawokakawo <evgeniymeruniko@gmail.com>
2025-01-24 07:12:52 +0000

    Add files via upload

---
+++ rickroll.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+def open_rick_roll():
+    # URL рикролла на YouTube
+    rick_roll_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    webbrowser.open(rick_roll_url)
+
+if __name__ == "__main__":
+    open_rick_roll()
+
